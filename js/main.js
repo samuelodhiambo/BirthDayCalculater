e9c1585c1d9883113fe70b68a5d5ed4e62a9485f
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-03-04 23:06:00 +0300

    logic: It will declare the constant variables for weekdays and names

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+const maleNames = {
+    "Sunday": "Kwasi",
+    "Monday": "Kwadwo",
+    "Tuesday": "Kwabena",
+    "Wednesday": "Kwaku",
+    "Thursday": "Yaw",
+    "Friday": "Kofi",
+    "Saturday": "Kwame"
+}
+
+const femaleNames = {
+    "Sunday": "Akosua",
+    "Monday": "Adwoa",
+    "Tuesday": "Abenaa",
+    "Wednesday": "Akua",
+    "Thursday": "Yaa",
+    "Friday": "Afua",
+    "Saturday": "Ama"
+}
